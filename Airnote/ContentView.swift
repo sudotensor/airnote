be3ea69012a49eba488c7abe90643b5a99f68567
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2022-01-22 15:07:41 +0000

    Setup xcode project

---
+++ Airnote/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Airnote
+//
+//  Created by Sudarshan Sreeram on 22/01/2022.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
